f6708b804941974519dc26ad8f190b4b3df873cb
Kyle Cierzan <kcierzan@gmail.com>
2024-02-13 19:39:18 -0500

    Use spaces

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,7 @@
 <a href="/login">Log in</a>
 
 <style lang="postcss">
-	:global(html) {
-		background-color: theme(colors.gray.100);
-	}
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
 </style>
