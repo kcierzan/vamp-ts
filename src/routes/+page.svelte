afaadd4e2e7bcb12d9c497244628974756352dfd
Kyle Cierzan <kcierzan@gmail.com>
2024-02-13 23:12:08 -0500

    Style login form

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,10 +5,15 @@
   export let data;
 </script>
 
-<Auth
-  supabaseClient={data.supabase}
-  view="magic_link"
-  redirectTo={`${data.url}/auth/callback`}
-  showLinks={false}
-  appearance={{ theme: ThemeSupa, style: { input: "color: #fff" } }}
-/>
+<div class="flex flex-row justify-center items-center h-screen">
+  <div class="flex flex-col w-1/3">
+    <h1 class="text-3xl bold text-center">Welcome to Vamp</h1>
+    <Auth
+      supabaseClient={data.supabase}
+      view="magic_link"
+      redirectTo={`${data.url}/auth/callback`}
+      showLinks={false}
+      appearance={{ theme: ThemeSupa, style: { input: "color: #222" } }}
+    />
+  </div>
+</div>
