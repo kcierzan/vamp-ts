1fa078043b95a39e3a02c9cd4e0a925cbcc95512
Kyle Cierzan <kcierzan@gmail.com>
2024-02-18 23:10:17 -0500

    Add dotenv script

---
+++ dotenv.sh
@@ -0,0 +1,7 @@
+#/usr/bin/env bash
+#
+set -e
+
+echo "PUBLIC_SUPABASE_URL=$(sudo supabase status 2>/dev/null | grep 'API URL' | cut -d: -f2- | tr -d ' ')" > .env
+echo "PUBLIC_SUPABASE_ANON_KEY=$(sudo supabase status 2>/dev/null | grep 'anon key' | cut -d: -f2- | tr -d ' ')" >> .env
+echo "envvars written to .env"
